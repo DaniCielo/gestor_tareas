01257071500d8d68655c55e4195418f5bf90b3d2
DaniCielo <cielomenendezdaniel@gmail.com>
2025-05-11 19:10:21 +0200

    Creación página inicial

--- main.py
+++ main.py
@@ -2,5 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)  # Aquí se encontrará el servidor web de Flask
 
+
+# La barra (el slash) se conoce como la página de inicio (página home)
+@app.route('/')
+def home():
+    return "Hello World"
+
+
 if __name__ == "__main__":
     app.run(debug=True)  # debug=True para que reinicie solo
