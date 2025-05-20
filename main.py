661d970bda610e88382ba8e263f3cd8254d44bc2
DaniCielo <cielomenendezdaniel@gmail.com>
2025-05-20 17:52:39 +0200

    Añadir tarea con actualizado de web

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ app = Flask(__name__)  # Aquí se encontrará el servidor web de Flask
 # La barra (el slash) se conoce como la página de inicio (página home)
 @app.route('/')
 def home():
-    return render_template("index.html")
+    todas_las_tareas = db.session.query(Tarea).all()  # Consulta en DB
+    # Las tareas se devuelven para incorporar al index.html
+    return render_template("index.html", lista_de_tareas=todas_las_tareas)
 
 
 @app.route('/crear-tarea', methods=['POST'])
@@ -16,7 +18,7 @@ def crear():
     tarea = Tarea(contenido=request.form['contenido-tarea'], hecha=False)
     db.session.add(tarea)  # Añadir el objeto de Tarea a la base de datos
     db.session.commit()  # Ejecutar la operación pendiente a la DB
-    return "Tarea guardada"  # Mensaje para ver a través del navegador
+    return redirect(url_for('home'))  # Lleva a la carga inicial de la función home
 
 
 if __name__ == "__main__":
